9d1aaa817fd908c0848413062d07d49c77700e60
Vitaliy Rudakovskiy <vitalirud03@gmail.com>
2026-04-07 15:49:36 +0300

    feat: garage page

--- src/app/features/garage/garage.ts
+++ src/app/features/garage/garage.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Button } from '@shared/ui';
 
 @Component({
 	selector: 'app-garage',
 	templateUrl: './garage.html',
 	styleUrl: './garage.scss',
+	imports: [Button],
 })
-export class Garage {}
+export class Garage {
+	isAddCarModalOpen = signal(false);
+
+	showAddCarModal(): void {
+		this.isAddCarModalOpen.set(true);
+	}
+
+	closeAddCarModal(): void {
+		this.isAddCarModalOpen.set(false);
+	}
+}
