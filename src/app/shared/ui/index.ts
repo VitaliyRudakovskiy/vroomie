798425e52e3da0702ad0207b880f463548249c52
Vitaliy Rudakovskiy <vitalirud03@gmail.com>
2026-04-14 16:20:37 +0300

    fix: build error

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -1,6 +1,5 @@
 export * from './autocomplete-dropdown';
 export * from './button';
 export * from './card';
-export * from './confirm-modal';
 export * from './loader';
 export * from './modal-wrapper';
