7dee7c61575ab4e2adfe30693e0893f292e7b91f
Vitaliy Rudakovskiy <vitalirud03@gmail.com>
2026-04-23 17:44:37 +0300

    feat: plans-page ui

--- src/app/features/plans-page/plans.ts
+++ src/app/features/plans-page/plans.ts
@@ -1,6 +1,8 @@
 import { Component, inject, input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Button } from '@shared/ui';
+import { selectCarInfo } from 'store/car-info/selectors';
 
 @Component({
 	selector: 'app-plans',
@@ -9,12 +11,16 @@ import { Button } from '@shared/ui';
 	imports: [Button],
 })
 export class Plans {
+	private readonly store = inject(Store);
 	private readonly router = inject(Router);
 	private readonly route = inject(ActivatedRoute);
 
 	carId = input.required<string>();
+	carInfo = this.store.selectSignal(selectCarInfo);
 
 	goBack(): void {
 		this.router.navigate(['..'], { relativeTo: this.route });
 	}
+
+	openPlanModal(): void {}
 }
